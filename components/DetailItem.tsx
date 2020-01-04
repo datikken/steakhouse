a20a3002156db0b7003a4f3daefac0e5b014135e
tikken <big_nick@gmail.com>
2020-01-04 15:04:46 +0300

    progressive images

--- components/DetailItem.tsx
+++ components/DetailItem.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import ProgressiveImage from '../components/ProgressiveImage';
 import {
     View,
     Text,
     StyleSheet,
-    ScrollView
+    ScrollView,
+    Dimensions
 } from 'react-native';
 
+const w = Dimensions.get('window');
+
 const DetailItem = ({details}) => {
+
+    console.log(details)
+
     return (
         <ScrollView>
             <View>
+                <ProgressiveImage
+                    thumbnailSource={{ uri: details.imageUrl }}
+                    source={{ uri: details.imageUrl }}
+                    style={{ width: w.width, height: w.width }}
+                    resizeMode="cover"
+                />
                 <Text style={styles.heading}>{details.steps}</Text>
                 <View style={styles.column}>
                     {details.ingredients.map(item => (
